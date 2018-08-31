01d58b7689b0c6043d6018d0432bee331bd2adc0
pjatgithub <pjatgithub@users.noreply.github.com>
2018-08-31 20:21:17 +0800

    Finish find the shortest path with A* of LAB 11

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,5 +1,8 @@
 package lab11.graphs;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -20,7 +23,9 @@ public class MazeAStarPath extends MazeExplorer {
 
     /** Estimate of the distance from v to the target. */
     private int h(int v) {
-        return -1;
+        int vX = maze.toX(v), vY = maze.toY(v);
+        int tX = maze.toX(t), tY = maze.toY(t);
+        return Math.abs(tX - vX) + Math.abs(tY - vY);
     }
 
     /** Finds vertex estimated to be closest to target. */
@@ -31,7 +36,32 @@ public class MazeAStarPath extends MazeExplorer {
 
     /** Performs an A star search from vertex s. */
     private void astar(int s) {
-        // TODO
+        Queue<Stop> queue = new PriorityQueue<>();
+        queue.offer(new Stop(s, s, 0, h(s)));
+
+        while (!queue.isEmpty()) {
+            Stop stop = queue.poll();
+
+            if (!marked[stop.to]) {
+                marked[stop.to] = true;
+                edgeTo[stop.to] = stop.from;
+                distTo[stop.to] = stop.dist;
+                announce();
+            } else {
+                continue;
+            }
+
+            if (stop.to == t) {
+                break;
+            }
+            // Relax
+            for (int neighbor : maze.adj(stop.to)) {
+                int dist = distTo[stop.to] + stop.weight;
+                if (distTo[neighbor] > dist) {
+                    queue.offer(new Stop(stop.to, neighbor, dist, h(neighbor)));
+                }
+            }
+        }
     }
 
     @Override
@@ -39,5 +69,21 @@ public class MazeAStarPath extends MazeExplorer {
         astar(s);
     }
 
+    static class Stop implements Comparable<Stop> {
+        int from, to, dist, h, weight;
+
+        public Stop(int from, int to, int dist, int h) {
+            this.from = from;
+            this.to = to;
+            this.dist = dist;
+            this.h = h;
+            weight = 1;
+        }
+
+        @Override
+        public int compareTo(Stop o) {
+            return Integer.compare(this.dist + this.h, o.dist + o.h);
+        }
+    }
 }
 
