06dc7c62ca21843aa0c78e4f0563e32c833be269
pjatgithub <pjatgithub@users.noreply.github.com>
2018-08-31 19:07:41 +0800

    Finish cycle detection with DFS of LAB 11

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Stack;
+
 /**
  *  @author Josh Hug
  */
@@ -16,9 +18,37 @@ public class MazeCycles extends MazeExplorer {
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        dfs();
     }
 
     // Helper methods go here
+    private void dfs() {
+        int numV = maze.V();
+        int[] edgeTo = new int[numV];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+
+        outer:
+        while (!stack.isEmpty()) {
+            int vertex = stack.pop();
+            marked[vertex] = true;
+            announce();
+
+            for (int neighbor : maze.adj(vertex)) {
+                if (!marked[neighbor]) {
+                    edgeTo[neighbor] = vertex;
+                    stack.push(neighbor);
+                } else if (neighbor != edgeTo[vertex]) {
+                    this.edgeTo[neighbor] = vertex;
+                    for (int w = vertex; w != neighbor; w = edgeTo[w]) {
+                        this.edgeTo[w] = edgeTo[w];
+                    }
+
+                    announce();
+                    break outer;
+                }
+            }
+        }
+    }
 }
 
