2aea6e4f9bd2caea58572891c9eea650dad49dbd
pjatgithub <pjatgithub@users.noreply.github.com>
2018-09-08 20:49:28 +0800

    Finish quick sort of Lab 12

--- lab12/QuickSort.java
+++ lab12/QuickSort.java
@@ -1,4 +1,7 @@
 import edu.princeton.cs.algs4.Queue;
+import org.junit.Assert;
+
+import java.util.Arrays;
 
 public class QuickSort {
     /**
@@ -6,8 +9,8 @@ public class QuickSort {
      *
      * The items in q2 will be catenated after all of the items in q1.
      */
-    private static <Item extends Comparable> Queue<Item> catenate(Queue<Item> q1, Queue<Item> q2) {
-        Queue<Item> catenated = new Queue<Item>();
+    private static <Item extends Comparable<? super Item>> Queue<Item> catenate(Queue<Item> q1, Queue<Item> q2) {
+        Queue<Item> catenated = new Queue<>();
         for (Item item : q1) {
             catenated.enqueue(item);
         }
@@ -18,7 +21,7 @@ public class QuickSort {
     }
 
     /** Returns a random item from the given queue. */
-    private static <Item extends Comparable> Item getRandomItem(Queue<Item> items) {
+    private static <Item extends Comparable<? super Item>> Item getRandomItem(Queue<Item> items) {
         int pivotIndex = (int) (Math.random() * items.size());
         Item pivot = null;
         // Walk through the queue to find the item at the given index.
@@ -44,16 +47,53 @@ public class QuickSort {
      * @param greater   An empty Queue. When the function completes, this queue will contain
      *                  all of the items in unsorted that are greater than the given pivot.
      */
-    private static <Item extends Comparable> void partition(
+    private static <Item extends Comparable<? super Item>> void partition(
             Queue<Item> unsorted, Item pivot,
             Queue<Item> less, Queue<Item> equal, Queue<Item> greater) {
-        // Your code here!
+        for (Item item : unsorted) {
+            int cmp = item.compareTo(pivot);
+            if (cmp < 0) {
+                less.enqueue(item);
+            } else if (cmp == 0) {
+                equal.enqueue(item);
+            } else {
+                greater.enqueue(item);
+            }
+        }
     }
 
     /** Returns a Queue that contains the given items sorted from least to greatest. */
-    public static <Item extends Comparable> Queue<Item> quickSort(
+    public static <Item extends Comparable<? super Item>> Queue<Item> quickSort(
             Queue<Item> items) {
-        // Your code here!
-        return items;
+        if (items.size() <= 1) {
+            return items;
+        }
+
+        Item pivot = getRandomItem(items);
+        Queue<Item> less = new Queue<>();
+        Queue<Item> equal = new Queue<>();
+        Queue<Item> greater = new Queue<>();
+        partition(items, pivot, less, equal, greater);
+
+        return catenate(catenate(quickSort(less), equal), quickSort(greater));
+    }
+
+    public static void main(String[] args) {
+        String[] expected = {"Monday", "Tuesday", "Wednesday",
+                "Thursday", "Friday", "Saturday", "Sunday"};
+        Queue<String> days = new Queue<>();
+
+        for (String day : expected) {
+            days.enqueue(day);
+        }
+
+        Arrays.sort(expected);
+        Queue<String> actual = quickSort(days);
+
+        Assert.assertEquals(expected.length, actual.size());
+        int index = 0;
+        for (String day : actual) {
+            Assert.assertEquals(expected[index++], day);
+        }
     }
 }
